9e4320f9da5d844657ed7a8f0eccbcc03d086110
nathangtg <nathanginting2007@gmail.com>
2026-06-17 16:27:17 +0800

    Added demo entrypoint for paper sim

---
+++ run_demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+NeSy-MBST Quick Demo
+Runs the complete Neuro-Symbolic MBST pipeline on the Autonomous Vehicle case study.
+"""
+from nesy_mbst.demo.autonomous_vehicle import run_av_demo
+
+if __name__ == "__main__":
+    run_av_demo()
