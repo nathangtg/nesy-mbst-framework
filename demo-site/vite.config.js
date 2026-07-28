76591abd0ecb75b751e1919c73124364b8c06c85
nathangtg <nathanginting2007@gmail.com>
2026-07-28 17:14:19 +0800

    Update VIte Config for deployment

--- demo-site/vite.config.js
+++ demo-site/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/nesy-mbst-framework/',
+  base: '/llm-mbst-research/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
